979389cdb5b615c21b1f2b925aed5cc948bbb0e9
Peter Paul Arasu <peterpaularasu@gmail.com>
2020-01-31 15:38:08 +0530

    Otp Page Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:foodie/pages/LoginPage.dart';
+import 'package:foodie/pages/OtpPage.dart';
 
 void main() => runApp(MyApp());
 
@@ -10,11 +11,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        textTheme: TextTheme(display1: TextStyle(color:Color(0xFF7B4E06) )),
         primaryColor: Color(0xFFB98105),
         accentColor: Color(0xFFFFF500),
         fontFamily: "Montseratt"
       ),
-      home: LoginPage()
+      home: OtpPage()
     );
   }
 }
---
+++ lib/pages/OtpPage.dart
@@ -0,0 +1,68 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:foodie/components/Button.dart';
+import 'package:pinput/pin_put/pin_put.dart';
+
+class OtpPage extends StatefulWidget {
+  @override
+  _OtpPageState createState() => _OtpPageState();
+}
+
+class _OtpPageState extends State<OtpPage> {
+  bool _visibility = true;
+  final spinKit = SpinKitThreeBounce(color: Color(0xFFFFF500),size: 30,);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          width: double.infinity,
+          color: Colors.white,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                "OTP verification",
+                style: TextStyle(color: Color(0xFF7B4E06)),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: Container(
+                    width: MediaQuery
+                        .of(context)
+                        .size
+                        .width - 80,
+                    child: PinPut(
+                      fieldsCount: 6,
+                      onSubmit: _otpSubmit,
+                      actionButtonsEnabled: false,
+                    )),
+              ),
+              SizedBox(height: 20,),
+              Visibility(
+                visible: _visibility,
+                child: mButton(
+                  listener: () {
+                    _otpSubmit();
+                  },
+                  text: "Verify",
+                ),
+              ),
+              Visibility(visible: !_visibility, child:spinKit,)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  _otpSubmit() {
+    setState(() {
+      _visibility = false;
+    });
+  }
+}
